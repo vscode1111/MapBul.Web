7fd7c8f50f80eba9702f15ceb9a023178ae20c5a
akryuchkov <akryuchkov@etreta.ru>
2016-07-18 18:38:51 +0300

    Проект сайта XIsland.

---
+++ MapBul.XIsland/App_Start/BundleConfig.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Optimization;
+
+namespace MapBul.XIsland
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/js/jquery").Include(
+                        "~/Scripts/jquery-3.1.0.min.js"));
+            bundles.Add(new ScriptBundle("~/css/style").Include(
+                        "~/Content/style.css"));
+            bundles.Add(new ScriptBundle("~/css/style").Include(
+                        "~/Content/mediaqueries.css"));
+        }
+    }
+}
---
+++ MapBul.XIsland/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MapBul.XIsland.MvcApplication" Language="C#" %>
